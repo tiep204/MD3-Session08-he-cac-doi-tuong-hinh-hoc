805ed6921c34f3639f325d5f300969dd541a8a05
tiep204 <dangtrantiep240604@gmail.com>
2023-06-17 20:53:36 +0700

    first commit

---
+++ src/ra/Cricle.java
@@ -0,0 +1,34 @@
+package ra;
+
+public class Cricle extends Shape {
+    private double radius = 1.0;
+
+    public Cricle() {
+    }
+
+    public Cricle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius= " + radius + ", which is a subclass of " + super.toString();
+    }
+}
---
+++ src/ra/Square.java
@@ -0,0 +1,43 @@
+package ra;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    public void setSide(double side) {
+        setWidth(side);
+        setLenght(side);
+    }
+
+
+
+    @Override
+    public void setWidth(double width) {
+        setSide(width);
+    }
+
+    public void setLength(double lenght) {
+        setSide(lenght);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+
+}
